057102c0051523f4812b0a8ac4179542d0b09443
Binh Nguyen <binh.nguyen.uw@rvc.renesas.com>
2016-03-18 11:24:23 +0700

    qt_hmi_demo: add fullscreen display for qmlvideo on taskbar
    Signed-off-by: Binh Nguyen <binh.nguyen.uw@rvc.renesas.com>

--- meta_rzg1/qt_hmi_demo/recipes-demo/launcher-taskbar/scripts/Qmlvideo-demo.sh
+++ meta_rzg1/qt_hmi_demo/recipes-demo/launcher-taskbar/scripts/Qmlvideo-demo.sh
@@ -2,5 +2,5 @@
 #qmlvideo need to use pulseaudio for multi video playing function
 export LD_PRELOAD=/usr/lib/libEGL.so
 pulseaudio --resample-method=speex-fixed-1 --exit-idle-time=-1 --start
-/usr/share/qt5/examples/multimedia/video/qmlvideo/qmlvideo
+/usr/share/qt5/examples/multimedia/video/qmlvideo/qmlvideo --fullscreen
 pulseaudio --kill
