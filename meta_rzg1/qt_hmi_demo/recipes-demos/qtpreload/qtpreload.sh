108743cf7d80c555ddcb07fae126d53654ac8017
Binh Nguyen <binh.nguyen.uw@rvc.renesas.com>
2016-04-01 18:28:30 +0700

    qt_hmi_demo: correct filename of qtpreload
    Signed-off-by: Binh Nguyen <binh.nguyen.uw@rvc.renesas.com>

--- meta_rzg1/qt_hmi_demo/recipes-demos/qtpreload/qtpreload.sh
+++ meta_rzg1/qt_hmi_demo/recipes-demos/qtpreload/qtpreload.sh
@@ -1,4 +1,4 @@
 export XDG_RUNTIME_DIR=/run/user/root
 export LD_PRELOAD=/usr/lib/libEGL.so
-/home/root/welcome-screen/Welcome_screen &
+/home/root/qtpreload/qtpreload &
 echo 2 > /proc/sys/kernel/printk
