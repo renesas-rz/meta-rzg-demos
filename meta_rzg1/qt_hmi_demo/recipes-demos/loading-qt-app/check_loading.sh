f4bcab5d94cf8039d763c04b3d4ff92beb669829
TungDang <tung.dang.jy@rvc.renesas.com>
2016-07-25 15:41:21 +0700

    Remove debug log and Vietnamese language note
    Signed-off-by: TungDang <tung.dang.jy@rvc.renesas.com>

--- meta_rzg1/qt_hmi_demo/recipes-demos/loading-qt-app/check_loading.sh
+++ meta_rzg1/qt_hmi_demo/recipes-demos/loading-qt-app/check_loading.sh
@@ -14,86 +14,86 @@
 		local temp=$1
 		local temp1=($temp)
 		result=${temp1[0]}
-		echo "Result get only one pid: ${result}"
+		#echo "Result get only one pid: ${result}"
 	}	
 	
 	Max_pid_qt_app() {
-		echo "Get max Qt app"
+		#echo "Get max Qt app"
 		if [ "${pid_qt_launch_demo}" ] ; then
 			Get_only_one_pid_app ${pid_qt_launch_demo}
 			if [  "${Max}" -lt "${result}" ] ; then
 				Max=${result}
-				echo "Max pid_qt_launch_demo: $Max"
+				#echo "Max pid_qt_launch_demo: $Max"
 			fi
 		fi
 		if [ "${pid_qt_everywhere_demo}" ]  ; then
 			Get_only_one_pid_app ${pid_qt_everywhere_demo}
 			if [  "${Max}" -lt "${result}" ] ; then
 				Max=${result}
-				echo "Max pid_qt_everywhere_demo: $Max"
+				#echo "Max pid_qt_everywhere_demo: $Max"
 			fi
 		fi
 		if [ "${pid_qt_cinematic_demo}" ]  ; then
 			Get_only_one_pid_app ${pid_qt_cinematic_demo}
 			if [  "${Max}" -lt "${result}" ] ; then
 				Max=${result}
-				echo "Max pid_qt_cinematic_demo: $Max"
+				#echo "Max pid_qt_cinematic_demo: $Max"
 			fi
 		fi
 		if [ "${pid_qt_qmlvideo}" ]  ; then
 			Get_only_one_pid_app ${pid_qt_qmlvideo}
 			if [  "${Max}" -lt "${result}" ] ; then
 				Max=${result}
-				echo "Max pid_qt_qmlvideo: $Max"
+				#echo "Max pid_qt_qmlvideo: $Max"
 			fi
 		fi
 		if [ "${pid_qt_qmlvideofx}" ]  ; then
 			Get_only_one_pid_app ${pid_qt_qmlvideofx}
 			if [  "${Max}" -lt "${result}" ] ; then
 				Max=${result}
-				echo "Max pid_qt_qmlvideofx: $Max"
+				#echo "Max pid_qt_qmlvideofx: $Max"
 			fi
 		fi
 		if [ "${pid_qt_webengine}" ]  ; then
 			Get_only_one_pid_app ${pid_qt_webengine}
 			if [  "${Max}" -lt "${result}" ] ; then
 				Max=${result}
-				echo "Max pid_qt_webengine: $Max"
+				#echo "Max pid_qt_webengine: $Max"
 			fi
 		fi		
 	}
 	
 running_app(){	
 	if  [ "${pid_qt_launch_demo}" ] || [ "${pid_qt_everywhere_demo}" ] || [ "${pid_qt_cinematic_demo}" ]  || [ "${pid_qt_qmlvideo}" ]  || [ "${pid_qt_qmlvideofx}" ] || [ "${pid_qt_webengine}" ]  ; then
-		echo "Ton tai qt-app "
+		#echo "Exist qt-app"
 		#Get max pid Qt app
 		Max_pid_qt_app
 
 		#Get pid of newest Loading app
 		if [ "$pid_Loading" ] ; then  # Loading is calling
-			echo "Ton tai Loading app"
-			echo "pid Loading : ${pid_Loading}"
+			#echo "Exist Loading app"
+			#echo "pid Loading : ${pid_Loading}"
 			check=(${pid_Loading})
 			check1=($check)
 			Get_only_one_pid_app ${pid_Loading}
 			pid_loading_app=${result}
-			echo "Max pid loading app: $pid_loading_app "
-			echo "--------------------"
+			#echo "Max pid loading app: $pid_loading_app "
+			#echo "--------------------"
 			
 			#Compare pid loading app and qt app
 			if [ ${pid_loading_app} -lt ${Max} ] ; then # app is calling
-				echo "pid loading app less than max  pid Qt app:  ${pid_loading_app} -lt ${Max}"
-				echo "killall loading app ${pid_loading_app}"
+				#echo "pid loading app less than max  pid Qt app:  ${pid_loading_app} -lt ${Max}"
+				#echo "killall loading app ${pid_loading_app}"
 				#kill 	${check1[0]}
 				killall Loading
 			else
-				echo "pid loading app large than max  pid Qt app"
+				#echo "pid loading app large than max  pid Qt app"
 			fi	
 		else 
-			echo "Ko ton tai loading app"
+			#echo "Not exist loading app"
 		fi
 	else
-		echo "Khong ton tai Qt app "
+		#echo "Not exist Qt app "
 	fi
 }
 
