320489c1f0c2a16f5bf77461541a9a24ab43f9fa
HungTran <hung.tran.jy@renesas.com>
2017-11-14 14:11:53 +0900

    Hide warning messages from qtpreload
    qtpreload simply pre-loads Qt modules. The purpose
    is to run demos faster (after pre-loaded Qt modules).
    
    Currently it shows some warning message about
    missing Qt modules (these modules are removed).
    The warning message has no harm at all, but will
    annoy user. Hide those messages in this patch.
    
    Signed-off-by: HungTran <hung.tran.jy@renesas.com>

--- meta-rzg1/qt-hmi-demo/recipes-demos/qtpreload/qtpreload.sh
+++ meta-rzg1/qt-hmi-demo/recipes-demos/qtpreload/qtpreload.sh
@@ -1,4 +1,4 @@
 export XDG_RUNTIME_DIR=/run/user/root
 export LD_PRELOAD=/usr/lib/libEGL.so
-/home/root/qtpreload/qtpreload &
+/home/root/qtpreload/qtpreload &>/dev/null &
 echo 2 > /proc/sys/kernel/printk
